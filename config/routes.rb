e1bff7d2bb51f3341b0777c321ffe3490a8eaa41
廣田隼 <hirotashun@hirotashun-no-MacBook-Pro.local>
2021-09-13 12:11:43 +0900

    message model migrate

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,10 @@ Rails.application.routes.draw do
     :passwords => 'users/passwords'
   }
 
-
   get 'users/show' => 'users#show'
   resources :users, only: [:show, :index]
+
+  get 'messages/index/:id' => "messages#index"
+  get 'messages/room/:user_id/:to_user_id' => "messages#roomshow"
+  resources :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_01_190253) do
+ActiveRecord::Schema.define(version: 2021_09_13_025316) do
+
+  create_table "messages", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
